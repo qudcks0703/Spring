7af6be913d8c32f9bb91af955e6bfed7d237589b
qudcks0703 <qudcks0703@naver.com>
2020-02-18 16:05:42 +0900

    Spring0218 afternoon study

--- spring/src/test/spring/bean/TestBean.java
+++ spring/src/test/spring/bean/TestBean.java
@@ -1,23 +1,21 @@
 package test.spring.bean;
 
-import javax.servlet.http.HttpServlet;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 //컨트롤러 어노테이션:컨트롤러에서 이클래스를 호출할수있는 빈이라고 알려줌
 @Controller
 //별 무조건 적어야함 안적으려면 안에도 꼭적어줘야함
 //?.do 하면 한글자만 가능.
-@RequestMapping("/0218/*")
-public class Hellobean{
+public class TestBean{
 
-	@RequestMapping("testaa")
-	public String hello() {
+	@RequestMapping(value="study",method = RequestMethod.GET)
+	public String Study() {
 		return "/WEB-INF/views/spring0218/hello.jsp";
 	}
-	@RequestMapping
-	public String test() {
+	@RequestMapping(method = RequestMethod.POST)
+	public String Exam() {
 		return "/WEB-INF/views/spring0218/test.jsp";
 	}
 }
